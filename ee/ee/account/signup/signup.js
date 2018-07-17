6ce4b6f8d0c6b4bca42bc66154e7842c7829ff99
aravindpanil <aravindpanil@gmail.com>
2018-07-17 12:34:01 +0530

    Remove unnecessary Comments and fix formatting

--- ee/ee/account/signup/signup.js
+++ ee/ee/account/signup/signup.js
@@ -1,14 +1,11 @@
-
 'use strict';
 
 var config = browser.params;
 var subs = require('../common/subSegment');
 
-
 describe('Sign up', function () {
 
     var data, signup;
-
     data = require('./signupdata');
     signup = require('../common/sign.common');
     var generateOTP = element(by.xpath('//button[@aria-label=\'Generate OTP\']'));
@@ -61,19 +58,30 @@ describe('Sign up', function () {
 
     function exploreMore(seg, subseg) {
         var segs = ['Cereals', 'Coffee', 'Vegetables', 'Yarn', 'Fabric', 'Apparel'];
+
         for (var k = 0; k < seg.length; k++) {
+
             var flag = 0;
+
             for (var i = 0; i < segs.length; i++) {
+
                 if (seg[k] === segs[i]) {
+
                     flag = 1;
+
                     if (subseg) {
+
                         var sub = getsubSegment(seg[k]);
                         var subsegdata = getsubSegmentData(seg[k], subseg);
+
                         if (subsegdata === 0)
                             return false;
+
                         for (var j = 0; j < subsegdata.length; j++) {
+
                             var flag1 = 0;
                             for (var z = 0; z < sub.length; z++) {
+
                                 if (subsegdata[j] === sub[z]) {
                                     flag1 = 1;
                                 }
@@ -90,8 +98,8 @@ describe('Sign up', function () {
         return true;
     }
 
-
     function validateSegment(trad, seg, subseg) {
+
         if (seg) {
             var segs = ['Rice', 'Coffee', 'Textiles', 'Fruits and Vegetables'];
 
@@ -132,8 +140,6 @@ describe('Sign up', function () {
         return false;
     }
 
-
-
     function selectSegment(trad, seg, subseg) {
         if (trad === 'Manufacturer') {
             seg.forEach(function (seg) {
@@ -152,7 +158,7 @@ describe('Sign up', function () {
                     flag = 0;
                     if (subseg) {
                         var showDetails = element(by.xpath('//label[text()=\'Show Details\' and ..//label[text()=\'' + seg + '\']]'));
-                        
+
                         showDetails.click();
                         var subsegData = getsubSegmentData(seg, subseg);
                         for (var i = 0; i < subsegData.length; i++) {
@@ -191,8 +197,8 @@ describe('Sign up', function () {
             });
             return;
         }
-        else{
-            done("No trading selected",null)
+        else {
+            done("No trading selected", null)
         }
     }
 
