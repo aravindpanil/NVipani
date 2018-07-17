a569e39c2028a4e57f6cdde1e297b3e92764223b
aravindpanil <aravindpanil@gmail.com>
2018-07-17 16:12:23 +0530

    Placeholder for Account configuration file

--- protractor.conf.js
+++ protractor.conf.js
@@ -6,77 +6,8 @@ var config = {
     baseUrl: 'http://staging.nvipani.com/#!/',
     onPrepare: function () {
         browser.driver.manage().window().maximize();
-        /* browser.get('#!/register');*/
-        /* browser.get('!#/register');*/
     },
-    suites: {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-        registration: ['ee/ee/account/signup/*.js',
-            'ee/ee/account/signin/*.js'],
-        contacts: ['e2e/contacts/createcontact/*.spec.js*',
-            'e2e/contacts/editcontact/*.spec.js*',
-            'e2e/contacts/creategroup/*.spec.js*'],
-        products: ['e2e/products/createproduct/*.spec.js',
-            'e2e/products/editproduct/*.spec.js',
-            'e2e/products/importproducts/*.spec.js'],
-        all: ['e2e/account/signup/*.spec.js',
-            'e2e/account/signin/*.spec.js',
-            'e2e/products/createproduct/*.spec.js',
-            'e2e/products/editproduct/*.spec.js',
-            'e2e/products/importproducts/*.spec.js',
-            'e2e/contacts/createcontact/*.spec.js*',
-            'e2e/contacts/editcontact/*.spec.js*',
-            'e2e/contacts/creategroup/*.spec.js*',
-            'e2e/businessuser/createbusinessuser/*.spec.js*',
-            'e2e/businessuser/editbusinessuser/*.spec.js*',
-            'e2e/businessunit/createbusinessunit/*.spec.js*',
-            'e2e/businessunit/editbusinessunit/*.spec.js*',
-            'e2e/updatecompanyinfo/*.spec.js*']
-||||||| parent of 39ae980... Integrate all modules of account into conf file
-        registration: ['ee/ee/account/signup/*.js',
-            'ee/ee/account/signin/*.js'],
-        contacts: ['e2e/contacts/createcontact/*.js*',
-            'e2e/contacts/editcontact/*.js*',
-            'e2e/contacts/creategroup/*.js*'],
-        products: ['e2e/products/createproduct/*.js',
-            'e2e/products/editproduct/*.js',
-            'e2e/products/importproducts/*.js'],
-        all: ['e2e/account/signup/*.js',
-            'e2e/account/signin/*.js',
-            'e2e/products/createproduct/*.js',
-            'e2e/products/editproduct/*.js',
-            'e2e/products/importproducts/*.js',
-            'e2e/contacts/createcontact/*.js*',
-            'e2e/contacts/editcontact/*.js*',
-            'e2e/contacts/creategroup/*.js*',
-            'e2e/businessuser/createbusinessuser/*.js*',
-            'e2e/businessuser/editbusinessuser/*.js*',
-            'e2e/businessunit/createbusinessunit/*.js*',
-            'e2e/businessunit/editbusinessunit/*.js*',
-            'e2e/updatecompanyinfo/*.js*']
-=======
-        registration: ['ee/ee/account/signup/signup.js',
-||||||| parent of 196e523... Fix suite name in conf file
-        registration: ['ee/ee/account/signup/signup.js',
-=======
-        account: ['ee/ee/account/signup/signup.js',
->>>>>>> 196e523... Fix suite name in conf file
-            'ee/ee/account/signin/signin.js',
-            'ee/ee/account/signin/forgotpassword.js'
-||||||| parent of 651c742... Temporary Changes to Integrate Contacts Suite
-        account: ['ee/ee/account/signup/signup.js',
-            'ee/ee/account/signin/signin.js',
-            'ee/ee/account/signin/forgotpassword.js'
-=======
-        account: ['ee/ee/account/signup/signup.js'
-            //'ee/ee/account/signin/signin.js',
-            //'ee/ee/account/signin/forgotpassword.js'
->>>>>>> 651c742... Temporary Changes to Integrate Contacts Suite
-        ],
->>>>>>> 39ae980... Integrate all modules of account into conf file
-    },
+    suites: {},
     jasmineNodeOpts: {
         isVerbose: true,
         showColors: true,
