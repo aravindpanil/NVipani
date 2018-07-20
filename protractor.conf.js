c2a5edda78a349639c5966dca00190366ba68718
aravindpanil <aravindpanil@gmail.com>
2018-07-20 11:51:31 +0530

    Clean up for accounts in conf file

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,6 +1,5 @@
 'use strict';
 
-// Protractor configuration
 var config = {
     troubleshoot: true,
     baseUrl: 'http://staging.nvipani.com/#!/',
@@ -14,23 +13,6 @@ var config = {
         defaultTimeoutInterval: 360000
     }
 };
-/*config.capabilities = {
-    //browserName: 'chrome',
-    browserName: 'chrome',
-        /!*
-         * Can be used to specify the phantomjs binary path.
-         * This can generally be ommitted if you installed phantomjs globally.
-         *!/
-        'phantomjs.binary.path': require('phantomjs-prebuilt').path,
-
-        /!*
-         * Command line args to pass to ghostdriver, phantomjs's browser driver.
-         * See https://github.com/detro/ghostdriver#faq
-         *!/
-        'phantomjs.ghostdriver.cli.args': ['--loglevel=Error']
-};*/
-
-
 
 config.multiCapabilities = [
     { browserName: 'chrome' }
