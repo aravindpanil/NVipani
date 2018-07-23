f63e7527f04440667989dbff734675772a1c556f
aravindpanil <aravindpanil@gmail.com>
2018-07-23 12:35:23 +0530

    Final bug fixes for order group actions

--- protractor.conf.js
+++ protractor.conf.js
@@ -7,12 +7,7 @@ var config = {
         browser.driver.manage().window().maximize();
     },
     suites: {
-        suite1: ['js file1',
-            'js file 2',
-            'js file 3'],
-
-        suite2: ['js file1',
-            'jsfile2']
+        suite1: ['ee/ee/order/groupactions/orderGroupActions.js']
     },
     
     jasmineNodeOpts: {
@@ -25,5 +20,5 @@ var config = {
 config.multiCapabilities = [
     { browserName: 'chrome' }
 ];
-const _config = config;
-export { _config as config };
+
+exports.config = config;
