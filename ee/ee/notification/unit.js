006d15789a011340aaa291284809d02bc920b1b9
aravindpanil <aravindpanil@gmail.com>
2018-07-27 14:47:45 +0530

    Add create user functionality

---
+++ ee/ee/notification/unit.js
@@ -0,0 +1,91 @@
+
+'use strict';
+
+var config = browser.params;
+
+describe('Create Business User', function () {
+
+    var i = 0;
+    var data = require('./cbu');
+    var sign = require('../account/common/sign.common');
+    var tab = element(by.xpath('//md-tab-item[text()=\'Business Units\']'));
+    var BunitType = element(by.id('businessUnitType'));
+    var addBunit = element(by.id('add-business-unit'));
+    var incharge = element(by.id('addBunitIncharge'));
+    var BunitName = element(by.model('businessUnit.name'));
+    var createBunit = element(by.id('createBunit'));
+    var closeaddBunit = element(by.id('closeAddBunit'));
+    var refreshBunit = element(by.id('refreshAddBunit'));
+
+    beforeAll(function(){
+        browser.get('');
+    })
+
+    afterEach(function () {
+        browser.sleep(1000);
+        sign.logout();
+    });
+
+    function BunitNameFunction(bunitName, done) {
+        if (bunitName) {
+            BunitName.sendKeys(bunitName);
+            done(null, null);
+        }
+        else
+            done("Missing Business Unit Name", BunitName);
+    }
+
+    function inchargeFunction(Incharge, done) {
+        incharge.click();
+        if (Incharge) {
+            var op = element(by.id(Incharge));
+            op.isPresent().then(function (res) {
+                if (res) {
+                    op.click();
+                    done(null, null);
+                }
+                
+                else {
+                    console.log("Using default incharge as" + Incharge + " not a business user");
+                    var option = element(by.id(data[0].username));
+                    option.click();
+                    done(null, null);
+                }
+            });
+        }
+        else {
+            var option = element(by.id(data[0].username));
+            option.click();
+            done(null, null);
+        }
+    }
+
+    data.forEach(function (data) {
+
+        it('should create a business unit', function () {
+            sign.login(data);
+            sign.companyProfile();
+            tab.click();
+            addBunit.click();
+            console.log("Test case ", i);
+            i++;
+
+            BunitNameFunction(data.businessUnitName, function (error, ele) {
+                if (error) {
+                    console.log(error);
+                    return;
+                }
+
+                inchargeFunction(data.incharge, function (error, ele) {
+                    if (error) {
+                        console.log(error);
+                        return;
+                    }
+                    createBunit.click();
+                    console.log("\n");
+                });
+            });
+
+        });
+    });
+});
---
+++ ee/ee/notification/user.js
@@ -0,0 +1,150 @@
+var config = browser.params;
+
+describe('Add Business User',function () {
+
+    var i=0;
+    var data=require('./customData');
+    var sign=require('../account/common/sign.common');
+
+    var tab=element(by.xpath('//md-tab-item[text()=\'Business Users\']'));
+    var addBusinessUser=element(by.id('add-business-user'));
+    var userGroup=element(by.xpath('//md-select[@ng-model=\'businessUser.userGroup\']'));
+    var emailId=element(by.model('businessUser.username'));
+    var createBusinessUser=element(by.id('create-business-user'));
+    var verifyBusinessUser=element(by.xpath('//div[@data-ng-show=\'success\' and ../../../@name=\'addBusinessUserForm\']'));
+    //div[@data-ng-show='success' and ../../../@name='addBusinessUserForm']
+    var closeButton=element(by.id('close-add-business-user'));
+
+    beforeAll(function () {
+    });
+
+    beforeEach(function () {
+        browser.get('');
+        
+    });
+
+    afterEach(function () {
+        //console.log("refreshedd")
+        browser.sleep(1000);
+        sign.logout()
+    });
+
+    afterAll(function () {
+    });
+
+    function VerifyBusinessUser(password,email){
+        console.log("entereddd")
+        var EC = protractor.ExpectedConditions;
+        var uname=element(by.xpath('//td[contains(text(),\''+email+'\')]'))
+        uname.click();
+        var resendLink=element(by.xpath('//h6[@ng-click=\'resendLink()\']'))
+        resendLink.click()
+        var extractText=element(by.xpath('//div[@data-ng-show=\'success\']'))
+        extractText.getText().then(function(txt){
+            console.log(txt)
+            var text=txt.split(" :");
+            console.log(text)
+            cancel=element(by.xpath('//button[@ng-click=\'cancel();\']'))
+            cancel.click();
+            sign.logout();
+            browser.get(text[1]);
+            var passWord = element(by.id('businessuserpassword'));
+            browser.wait(EC.visibilityOf(passWord), 5000);
+            passWord.sendKeys(password);
+            var activateButton=element(by.id('activate'));
+            activateButton.click();
+            //sign.logout();
+        });
+        
+
+        
+    }
+
+    function userGroupFunction(Usergroup,done){
+        //console.log("gp anme")
+        userGroup.click();
+        if(Usergroup){
+        if(userGroup.isDisplayed()){
+            var selectusergroup=element(by.id(Usergroup));
+            sign.isClickable(selectusergroup,function (error,ele) {
+                if(ele) {
+                    selectusergroup.click();
+                    done(null,null);
+                }
+                else
+                    done(error,ele);
+            });
+        }
+        
+        }
+        else{
+            //closeButton.click()
+            done("missing type")
+    }
+    }
+    function emailFunction(email,done){
+        if(email){
+            emailId.sendKeys(email);
+            emailId.getAttribute('aria-invalid').then(function (attr) {
+                if(attr === 'true')
+                    emailId.clear().then(function () {
+                        emailId.sendKeys('');
+                        done("Invalid Email ",email);
+                    });
+                else
+                    done(null,null);
+            });
+        }
+        else
+            done("Missing Email ");
+    }
+
+    data.forEach(function (data) {
+        
+        it('should add a Business User', function () {
+            console.log("Test "+i);
+            i++;
+            browser.get('')
+            sign.login(data)
+            sign.companyProfile()
+            tab.click();
+            
+            addBusinessUser.click();    
+            userGroupFunction(data.usergroup,function (error,ele) {
+                if(error){
+                    console.log(error);
+                    //closeButton.click()
+                    sign.logout()
+                    console.log("signed out")
+                    return;
+                }
+                console.log("finished 1");
+
+                emailFunction(data.email,function (error,ele) {
+                    if(error){
+                        console.log(error);
+                        return;
+                    }
+                    console.log("finished 2");
+                    sign.isClickable(createBusinessUser,function (error,ele) {
+                        if(ele){
+                            createBusinessUser.click();
+                            console.log("button clickedd")
+                            
+                            closeButton.click()
+                            VerifyBusinessUser(data.buserpwd,data.email)
+                        }
+                        else {
+                            console.log(error);
+                            return;
+                        }
+                        
+
+                    });
+
+                });
+            });
+        });
+    });
+
+});
