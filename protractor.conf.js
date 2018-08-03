8f94ad4211616efbec33cf8f58bab04ff4064d82
samagnatalanki <samagnatalanki@gmail.com>
2018-08-03 12:34:43 +0530

    temporary json files

--- protractor.conf.js
+++ protractor.conf.js
@@ -7,7 +7,14 @@ var config = {
         browser.driver.manage().window().maximize();
     },
     suites: {
-        notification: ['ee/ee/offer/createoffer/createOffer.js']
+        notification: [//'ee/ee/notification/user.js',
+        //'ee/ee/notification/unit.js',
+        //'ee/ee/product/createproduct/createProduct.js',
+        //'ee/ee/notification/createContact.js',
+        //'ee/ee/offer/createoffer/createOffer.js',
+        
+        'ee/ee/notification/createOrder.js',
+        'ee/ee/notification/notification.js']
     },
     
     jasmineNodeOpts: {
